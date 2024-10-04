643c86b4d7288c0581371f7a48b80338a9461116
Amit Sharma <amitsharma@Amits-MacBook-Pro.local>
2024-10-04 13:16:08 +0530

    quiz game

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,91 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+  <div id="app">
+
+    <ScoreBoard  :correctCount="this.correctCount" :wrongCount="this.wrongCount"/>
+
+    <template v-if="this.question">
+      <h1 v-html="this.question">
+      </h1>
+
+      <template v-for="(answer, index) in this.answers" :key="index">
+        <input type="radio" name="options" :value="answer" v-model="this.chosenAnswer" :disabled="this.answerSubmitted">
+        <label v-html="answer"></label>
+        <br>
+      </template>
+
+      <button v-if="!this.answerSubmitted" @click="this.submitAnswer()" class="send" type="button">Send</button>
+
+      <section v-if="this.answerSubmitted" class="result">
+        <h4 v-if="this.chosenAnswer == this.correctAnswer" v-html="'&#9989; Congratulations, the answer ' + this.correctAnswer + ' is correct.'">
+          
+        </h4>
+
+        <h4 v-else v-html="'&#10060; I`m Sorry, you picked the wrong answer, The correct is ' + this.correctAnswer + ' .'">
+           
+        </h4>
+        <button @click="this.getQuestion()" class="send" type="button">Next Question</button>
+      </section>
+    </template>
+   
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ScoreBoard from '@/components/ScoreBoard.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ScoreBoard
+  },
+  data(){
+    return {
+      question: null,
+      incorrectAnswers: null,
+      correctAnswer: null,
+      chosenAnswer: null,
+      answerSubmitted: false,
+      correctCount: 0,
+      wrongCount: 0
+    }
+  },
+  computed: {
+    answers() {
+      let answers = JSON.parse(JSON.stringify(this.incorrectAnswers));
+      answers.splice(Math.round(Math.random() * answers.length), 0, this.correctAnswer);
+      return answers;
+    }
+  },
+  methods: {
+    submitAnswer() {
+      if(!this.chosenAnswer) {
+        alert('Pick one of the options');
+      } else {
+        this.answerSubmitted = true;
+        if(this.correctAnswer == this.chosenAnswer) {
+          this.correctCount++;
+        } else {
+          this.wrongCount++;
+        }
+      }
+    },
+    getQuestion() {
+      this.question = null;
+      this.incorrectAnswers = null;
+      this.correctAnswer = null;
+      this.answerSubmitted = false;
+      this.chosenAnswer = null;
+
+      this.axios.get('https://opentdb.com/api.php?amount=1&category=18').then((response) => {
+      this.question = response.data.results[0].question;       
+      this.incorrectAnswers = response.data.results[0].incorrect_answers;       
+      this.correctAnswer = response.data.results[0].correct_answer; 
+    })
+    }
+  },
+  created() {
+    this.getQuestion();
   }
 }
 </script>
@@ -21,6 +97,22 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 60px auto;
+  max-width: 960px;
+  input[type=radio] {
+    margin: 12px 4px;
+    cursor: pointer;
+  }
+
+  button.send {
+    margin-top: 12px;
+    height: 40px;
+    min-width: 120px;
+    padding: 0 16px;
+    color: #fff;
+    background-color: #1867c0;
+    border: 1px solid #1867c0;
+    cursor: pointer;
+  }
 }
 </style>
---
+++ src/components/ScoreBoard.vue
@@ -0,0 +1,27 @@
+<template>
+    <section class="score">
+        Player <span>{{ this.correctCount }}</span> X <span>{{ this.wrongCount }}</span> Computer
+    </section>
+</template>
+
+<script>
+    export default {
+        props: ['correctCount', 'wrongCount']
+    }
+</script>
+
+<style lang="scss">
+    section.score {
+        border-bottom: 1px solid black;
+        padding: 24px;
+        font-size: 18px;
+        span {
+            padding: 8px;
+            font-weight: bold;
+            border: 1px solid black;
+        }
+    }
+    section {
+        display: block;
+    }
+</style>
